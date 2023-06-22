e7e9830b024f519e7707e9a4b94b77331145b9c1
Nishant5789 <nishantbhandari218@gmail.com>
2023-06-22 23:02:07 +0530

    Feat : added removeitem and updateitem funtionality  in cart

--- src/features/cart/cartApi.js
+++ src/features/cart/cartApi.js
@@ -1,9 +1,17 @@
 import axios from "axios";
+import { getUserId } from "../../app/constant";
 
-export function getCartItmes(userId) {
-    return axios.get(`http://localhost:8080/cart/${userId}/`)
+export function getCartItmes() {
+    return axios.get(`http://localhost:8080/cart/${getUserId()}`)
 }
 export function addItem(cartItem) {
     return axios.post(`http://localhost:8080/cart/`, cartItem);
 }
+export function updateItem(updatecartItem, cartItemId) {
+    return axios.patch(`http://localhost:8080/cart/${cartItemId}`, updatecartItem);
+}
+export function removeItem(cartItemId) {
+    return axios.delete(`http://localhost:8080/cart/${cartItemId}`);
+}
+
 
