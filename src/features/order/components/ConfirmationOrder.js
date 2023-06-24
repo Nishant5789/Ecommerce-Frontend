7dc71c4470a4aa67280b830801076ac8f6f05856
Nishant5789 <nishantbhandari218@gmail.com>
2023-06-24 22:57:13 +0530

    navbar profile menu issue

--- src/features/order/components/ConfirmationOrder.js
+++ src/features/order/components/ConfirmationOrder.js
@@ -7,18 +7,13 @@ const ConfirmationOrder = ({orderID}) => {
 
   const dispatch = useDispatch();
   const {confirmationId} = useParams();
-  
-
-  // useEffect(()=>{
-    
-  // },[])
 
   return (
     <div className='h-screen flex justify-center items-center'>
     <div className='bg-slate-300  sm:max-w-7xl max-w-xl  py-4 md:p-20 rounded-lg shadow-2xl'>
       <div>
         <h1 className='text-center text-purple-700 font-bold text-3xl'>Your order is confirmed: {confirmationId}</h1>
-        <p className='text-center text-2xl'>Check Your Order Status <Link to="/user/order"><span className='border-b-2 border-yellow-400 font-bold hover:text-yellow-300'>UserProfile{'->'}order</span></Link> </p>
+        <p className='text-center text-2xl'>Check Your Order Status <Link to="/order"><span className='border-b-2 border-yellow-400 font-bold hover:text-yellow-300'>UserProfile{'->'}order</span></Link> </p>
       </div>
       <div className='flex justify-center mt-4'>
        <Link to='/'> <button className='bg-purple-600 hover:bg-purple-700 active:bg-purple-400 text-white px-4 py-2 rounded-md'>Back to home Page</button></Link>
